35012bd1a712ddfa7fcdfa44846a9bbbe164be49
harshv5094 (Local User) <harshv5094@gmail.com>
2024-01-08 19:03:36 +0530

    feat: Adding new rules and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,20 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '1440px'
+    },
     extend: {
+      boxShadow: {
+        custom: 'hsl(0, 0%, 7%) 8px 5px'
+      },
       colors: {
-        yellow: hsl(47, 88%, 63%),
-        white: hsl(0, 0%, 100%),
-        grey: hsl(0, 0%, 50%),
-        black: hsl(0, 0%, 7%)
+        yellow: 'hsl(47, 88%, 63%)',
+        white: 'hsl(0, 0%, 100%)',
+        grey: 'hsl(0, 0%, 50%)',
+        black: 'hsl(0, 0%, 7%)'
       }
     }
   },
